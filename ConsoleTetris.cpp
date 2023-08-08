221a2690a39cde6f676ef8d5f5ca20ca615c63a4
Mayur <mayurjreddy@gmail.com>
2023-08-08 12:42:07 -0400

    Initial play field render

--- ConsoleTetris.cpp
+++ ConsoleTetris.cpp
@@ -2,19 +2,128 @@
 //
 
 #include <iostream>
+#include <vector>
+#include <Windows.h>
+#include <memory>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace std;
+
+wstring tetrisBlock[7];
+int fieldWidth = 12;
+int fieldHeight = 18;
+vector<uint8_t> field;
+
+int screenWidth = 120;
+int screenHeight = 40;
+
+
+int rotate(int x, int y, int angle) {
+	switch (angle % 4) {
+	case 0: return y * 4 + x;
+	case 1: return 12 + y - (x * 4);
+	case 2: return 15 - (y * 4) - x;
+	case 3: return 3 - y + (x * 4);
+	}
+
+	return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+bool validFit(int tetrisblock, int rotation, int locx, int locy) {
+	for (int x = 0; x < 4; x++) {
+		for (int y = 0; y < 4; y++) {
+			// get index of tetris block according to its rotation
+			int index = rotate(locx, locy, rotation);
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+			// get index of block inside the play field
+			int findex = (locy + y) * fieldWidth + (locx + x);
+
+			if (locx + x >= 0 && locx + x < fieldWidth) {
+				if (locy + y >= 0 && locy + y < fieldHeight) {
+					if (tetrisBlock[tetrisblock][index] == L'X' && field[findex] != 0)
+						return false; //  fail on first hit
+				}
+			}
+		}
+	}
+
+	return true;
+}
+int main()
+{
+	//// try to get dynamically screen width and height
+	//try {
+
+	//}
+	//catch (exception const& e) {
+	//	cout << e.what() << endl;
+	//}
+
+	tetrisBlock[0].append(L"..X...X...X...X."); 
+	tetrisBlock[1].append(L"..X..XX...X.....");
+	tetrisBlock[2].append(L".....XX..XX.....");
+	tetrisBlock[3].append(L"..X..XX..X......");
+	tetrisBlock[4].append(L".X...XX...X.....");
+	tetrisBlock[5].append(L".X...X...XX.....");
+	tetrisBlock[6].append(L"..X...X..XX.....");
+
+	//field = vector<uint8_t>(fieldWidth * fieldHeight); // play field buffer
+
+	field.resize(fieldWidth * fieldHeight);
+
+	for (int x = 0; x < fieldWidth; x++) { // boundary 
+		for (int y = 0; y < fieldHeight; y++) {
+			field[y * fieldWidth + x] = (x == 0 || x == fieldWidth - 1
+				|| y == fieldHeight - 1) ? 9 : 0;
+		}
+	}
+
+	//vector<wchar_t> sc 
+	//wchar_t* screen = new wchar_t[screenWidth, screenHeight];
+	vector<wchar_t> screen(screenWidth * screenHeight, L' ');
+	for (int i = 0; i < screenWidth * screenHeight; i++) screen[i] = L' ';
+
+	HANDLE console = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
+	SetConsoleActiveScreenBuffer(console);
+	DWORD bytesWritten = 0;
+
+	bool gameOver = false;
+
+	int currentPiece = 0;
+	int currentRotation = 0;
+	int currentX = fieldWidth / 2;
+	int currentY = 0;
+
+	while (!gameOver) {
+		// game timer
+		
+		// game input
+
+		// game logic
+
+		// render output
+
+		// draw board
+		for (int x = 0; x < fieldWidth; x++) {
+			for (int y = 0; y < fieldHeight; y++) {
+				screen[(y + 2) * screenWidth + (x + 2)] = L" ABCDEFG=#"[field[y * fieldWidth + x]];
+			}
+		}
+
+		//// draw falling block
+		//for (int x = 0; x < 4; x++) {
+		//	for (int y = 0; y < 4; y++) {
+		//		if (tetrisBlock[currentPiece][rotate(x, y, currentRotation)] == L'X') {
+		//			screen[(currentY + y + 2) * screenWidth + ]
+		//		}
+		//	}
+		//}
+
+		WriteConsoleOutputCharacter(console, screen.data(), screenWidth * screenHeight, {0, 0}, &bytesWritten);
+	}
+
+	// Display frame
+
+	//std::cout << "Hello" << endl;
+
+	return 0;
+}
